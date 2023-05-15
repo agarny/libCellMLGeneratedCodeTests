d518a915cca186bb04d2e15d402d5d05719bfca5
Alan Garny <agarny@hellix.com>
2023-05-15 17:10:09 +1200

    CMake: slight improvements to the way we configure our C and Python files.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -19,37 +19,59 @@ function(build_executable EXECUTABLE TYPE)
         cmake_parse_arguments(ARG "${OPTIONS}" "${ONE_VALUE_KEYWORDS}" "" ${ARGN})
 
         if("${ARG_ENDING_POINT}" STREQUAL "")
-            set(ARG_ENDING_POINT 1000.0)
+            set(ENDING_POINT 1000.0)
+        else()
+            set(ENDING_POINT ${ARG_ENDING_POINT})
         endif()
 
         if("${ARG_POINT_INTERVAL}" STREQUAL "")
-            set(ARG_POINT_INTERVAL 1.0)
-        endif()
-
-        if("${ARG_OUTPUT_POINTS}" STREQUAL "")
-            set(ARG_USE_OUTPUT_POINTS false)
+            set(POINT_INTERVAL 1.0)
         else()
-            set(ARG_USE_OUTPUT_POINTS true)
-        endif()
-
-        set(ARG_OUTPUT_POINTS_CPP "{${ARG_OUTPUT_POINTS}}")
-        set(ARG_OUTPUT_POINTS_PYTHON "[${ARG_OUTPUT_POINTS}]")
-
-        if(ARG_SKIP_FIRST_OUTPUT_POINT)
-            set(ARG_SKIP_FIRST_OUTPUT_POINT true)
-        else()
-            set(ARG_SKIP_FIRST_OUTPUT_POINT false)
+            set(POINT_INTERVAL ${ARG_POINT_INTERVAL})
         endif()
 
         if("${ARG_RELATIVE_TOLERANCE}" STREQUAL "")
-            set(ARG_RELATIVE_TOLERANCE 1.0e-7)
+            set(RELATIVE_TOLERANCE 1.0e-7)
+        else()
+            set(RELATIVE_TOLERANCE ${ARG_RELATIVE_TOLERANCE})
         endif()
 
         if("${ARG_ABSOLUTE_TOLERANCE}" STREQUAL "")
-            set(ARG_ABSOLUTE_TOLERANCE 1.0e-7)
+            set(ABSOLUTE_TOLERANCE 1.0e-7)
+        else()
+            set(ABSOLUTE_TOLERANCE ${ARG_ABSOLUTE_TOLERANCE})
+        endif()
+
+        set(OUTPUT_POINTS "{${ARG_OUTPUT_POINTS}}")
+
+        if(ARG_SKIP_FIRST_OUTPUT_POINT)
+            set(SKIP_FIRST_OUTPUT_POINT true)
+        else()
+            set(SKIP_FIRST_OUTPUT_POINT false)
+        endif()
+
+        if("${ARG_OUTPUT_POINTS}" STREQUAL "")
+            set(USE_OUTPUT_POINTS false)
+        else()
+            set(USE_OUTPUT_POINTS true)
         endif()
 
         configure_file(../main.ode.cpp.in ${MAIN_CPP})
+
+        set(OUTPUT_POINTS "[${ARG_OUTPUT_POINTS}]")
+
+        if(ARG_SKIP_FIRST_OUTPUT_POINT)
+            set(SKIP_FIRST_OUTPUT_POINT True)
+        else()
+            set(SKIP_FIRST_OUTPUT_POINT False)
+        endif()
+
+        if("${ARG_OUTPUT_POINTS}" STREQUAL "")
+            set(USE_OUTPUT_POINTS False)
+        else()
+            set(USE_OUTPUT_POINTS True)
+        endif()
+
         configure_file(../main.ode.py.in ${MAIN_PY})
     elseif("${TYPE}" STREQUAL "ALGEBRAIC")
         set(ONE_VALUE_KEYWORDS
@@ -59,23 +81,23 @@ function(build_executable EXECUTABLE TYPE)
 
         cmake_parse_arguments(ARG "" "${ONE_VALUE_KEYWORDS}" "" ${ARGN})
 
-        set(ARG_INITIAL_VARIABLE_VALUES_GUESSES_CPP "{${ARG_INITIAL_VARIABLE_VALUES_GUESSES}}")
-
-        string(REPLACE "{" "" ARG_INITIAL_VARIABLE_VALUES_GUESSES "${ARG_INITIAL_VARIABLE_VALUES_GUESSES}")
-        string(REPLACE "}" "" ARG_INITIAL_VARIABLE_VALUES_GUESSES "${ARG_INITIAL_VARIABLE_VALUES_GUESSES}")
-        string(REPLACE "\"," "\":" ARG_INITIAL_VARIABLE_VALUES_GUESSES "${ARG_INITIAL_VARIABLE_VALUES_GUESSES}")
-
-        set(ARG_INITIAL_VARIABLE_VALUES_GUESSES_PYTHON "{${ARG_INITIAL_VARIABLE_VALUES_GUESSES}}")
-
-        set(ARG_FINAL_VARIABLE_VALUES_CPP "{${ARG_FINAL_VARIABLE_VALUES}}")
-
-        string(REPLACE "{" "" ARG_FINAL_VARIABLE_VALUES "${ARG_FINAL_VARIABLE_VALUES}")
-        string(REPLACE "}" "" ARG_FINAL_VARIABLE_VALUES "${ARG_FINAL_VARIABLE_VALUES}")
-        string(REPLACE "\"," "\":" ARG_FINAL_VARIABLE_VALUES "${ARG_FINAL_VARIABLE_VALUES}")
-
-        set(ARG_FINAL_VARIABLE_VALUES_PYTHON "{${ARG_FINAL_VARIABLE_VALUES}}")
+        set(INITIAL_VARIABLE_VALUES_GUESSES "{${ARG_INITIAL_VARIABLE_VALUES_GUESSES}}")
+        set(FINAL_VARIABLE_VALUES "{${ARG_FINAL_VARIABLE_VALUES}}")
 
         configure_file(../main.algebraic.cpp.in ${MAIN_CPP})
+
+        string(REPLACE "{" "" INITIAL_VARIABLE_VALUES_GUESSES "${ARG_INITIAL_VARIABLE_VALUES_GUESSES}")
+        string(REPLACE "}" "" INITIAL_VARIABLE_VALUES_GUESSES "${INITIAL_VARIABLE_VALUES_GUESSES}")
+        string(REPLACE "\"," "\":" INITIAL_VARIABLE_VALUES_GUESSES "${INITIAL_VARIABLE_VALUES_GUESSES}")
+
+        set(INITIAL_VARIABLE_VALUES_GUESSES "{${INITIAL_VARIABLE_VALUES_GUESSES}}")
+
+        string(REPLACE "{" "" FINAL_VARIABLE_VALUES "${ARG_FINAL_VARIABLE_VALUES}")
+        string(REPLACE "}" "" FINAL_VARIABLE_VALUES "${FINAL_VARIABLE_VALUES}")
+        string(REPLACE "\"," "\":" FINAL_VARIABLE_VALUES "${FINAL_VARIABLE_VALUES}")
+
+        set(FINAL_VARIABLE_VALUES "{${FINAL_VARIABLE_VALUES}}")
+
         configure_file(../main.algebraic.py.in ${MAIN_PY})
     else()
         message(FATAL_ERROR "The type of the executable must be either `ODE` or `ALGEBRAIC`.")
